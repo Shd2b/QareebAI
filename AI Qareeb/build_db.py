6b49273cf5877727ba4cc6bfceec59e35fdff60d
Shahad <shahad-b@hotmail.com>
2026-05-07 23:25:11 +0300

    final fix

---
+++ AI Qareeb/build_db.py
@@ -0,0 +1,54 @@
+import os
+from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# تحميل الملفات
+def load_txt_files(folder_path):
+    documents = []
+    for file in os.listdir(folder_path):
+        if file.endswith(".txt"):
+            with open(os.path.join(folder_path, file), "r", encoding="utf-8") as f:
+                documents.append(f.read())
+    print(f"✅ Loaded {len(documents)} files")
+    return documents
+# تحميل البيانات
+documents = load_txt_files("./docs")
+
+# تقسيم النصوص
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1200,
+    chunk_overlap=200,
+)
+
+chunks = []
+for doc in documents:
+    chunks.extend(splitter.split_text(doc))
+
+print(f"✅ Created {len(chunks)} chunks")
+
+#  تحويل إلى مستندات
+processed_chunks = [
+    Document(page_content=chunk, metadata={"source": "txt"})
+    for chunk in chunks
+]
+
+print(f"✅ Created {len(processed_chunks)} documents")
+
+# Embedding
+embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+
+# إنشاء DB
+print("🚀 Creating vector store...")
+
+Chroma.from_documents(
+    documents=processed_chunks,
+    embedding=embedding_model,
+    persist_directory="dbv1/chroma_db"
+)
+
+print("✅ DB created successfully!")
--- AI Qareeb/main.py
+++ AI Qareeb/main.py
@@ -1,4 +1,6 @@
+from xml.dom.minidom import Document
 from fastapi import FastAPI
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_chroma import Chroma
@@ -10,11 +12,61 @@ os.environ["OPENAI_API_KEY"] = "sk-proj-dYWQu-qfufnsoqN9cuKsO3_GuzAqv7yL1ngHjOSo
 
 app = FastAPI()
 
+PERSIST_DIR = "dbv1/chroma_db"
+
+# تحميل ملفات الاسعافات الأولية
+def load_txt_files(folder_path):
+    documents = []
+    for file in os.listdir(folder_path):
+        if file.endswith(".txt"):
+            with open(os.path.join(folder_path, file), "r", encoding="utf-8") as f:
+                documents.append(f.read())
+    print(f"✅ Loaded {len(documents)} files")
+    return documents
+
+
+# إعداد embedding 
 embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
-db = Chroma(
-    persist_directory="dbv1/chroma_db",
-    embedding_function=embedding_model
-)
+
+# بناء أو تحميل DB
+if not os.path.exists(PERSIST_DIR):
+    print("Building DB...")
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    DOCS_PATH = os.path.join(BASE_DIR, "docs")
+
+    documents = load_txt_files(DOCS_PATH)
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1200,
+        chunk_overlap=200,
+    )
+
+    chunks = []
+    for doc in documents:
+        chunks.extend(splitter.split_text(doc))
+
+    print(f"Created {len(chunks)} chunks")
+
+    processed_chunks = [
+        Document(page_content=chunk, metadata={"source": "txt"})
+        for chunk in chunks
+    ]
+
+    db = Chroma.from_documents(
+        documents=processed_chunks,
+        embedding=embedding_model,
+        persist_directory=PERSIST_DIR
+    )
+
+    print("DB created successfully!")
+
+else:
+    print("Loading existing DB...")
+    db = Chroma(
+        persist_directory=PERSIST_DIR,
+        embedding_function=embedding_model
+    )
 
 class QuestionRequest(BaseModel):
     question: str
